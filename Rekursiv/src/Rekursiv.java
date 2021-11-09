545c7def7767678a0fa2b6844891e2a29b5d7979
Johannes <jonny.egger004@gmail.com>
2021-11-09 21:23:27 +0100

    hi

--- Rekursiv/src/Rekursiv.java
+++ Rekursiv/src/Rekursiv.java
@@ -1,8 +1,14 @@
 
 public class Rekursiv {
 	static int wert = 0;
-	public static void main(String[] args) {		
-		System.out.println(Summieren(3));
+	public static void main(String[] args) {
+		final long timeStart = System.nanoTime();
+		
+		System.out.println(Summieren(10000));
+		 	  
+	    final long timeEnd = System.nanoTime();
+	    System.out.println("Verlaufszeit der Schleife: " + (timeEnd - timeStart)/1000 + " Mikrosek.");
+		
 	}
 
 	public static int Summieren(int number) {				
